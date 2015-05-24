5035b87a50ec02d49bbe60e453048684956fe129
Manoj Kulkarni <manoj.kulkarni.3011@gmail.com>
2015-05-24 09:29:40 +0530

    Initial Commit

---
+++ CustomInlineHelper/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace CustomInlineHelper.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Fruits = new string[] {"Apple", "Mango", "Lemon"};
+            ViewBag.Flowers = new string[] { "Rose", "Jasmine", "Sunflower" };
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CustomInlineHelper/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomInlineHelper.Startup))]
+namespace CustomInlineHelper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
