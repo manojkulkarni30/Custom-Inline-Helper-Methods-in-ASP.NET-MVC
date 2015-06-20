a0f462a75d2c4c3c47c9f829710fac4c414434f2
manojkulkarni30 <manojkulkarni30@gmail.com>
2015-06-20 18:58:57 +0530

    Added the code for external helper method

---
+++ CustomInlineHelper/Helpers/ExternalHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace CustomInlineHelper.Helpers
+{
+    public static class ExternalHelper
+    {
+        public static MvcHtmlString ListItems(this HtmlHelper htmlHelper, string[] items)
+        {
+            var ulElement = new TagBuilder("ul");
+            foreach (var item in items)
+            {
+                var liElement = new TagBuilder("li");
+                liElement.SetInnerText(item);
+                ulElement.InnerHtml += liElement.ToString();
+            }
+            return new MvcHtmlString(ulElement.ToString());
+        }
+    }
+}
